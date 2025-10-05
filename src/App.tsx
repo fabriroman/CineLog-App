490b93a110aa59c85707d33151da2f0d27c34db9
fabri10roman <fabri11roman.fr@gmail.com>
2025-10-05 01:04:01 -0300

    implement auth system with login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./features/movies/pages/HomePage";
+import LoginPage from "./features/auth/pages/LoginPage";
+import { RequireGuest } from "./features/auth/guards/RequireGuest";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route
+          path="/login"
+          element={
+            <RequireGuest>
+              <LoginPage />
+            </RequireGuest>
+          }
+        />
       </Routes>
     </>
   );
