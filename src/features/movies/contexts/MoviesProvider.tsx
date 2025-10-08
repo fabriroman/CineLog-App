d6cd5a2f219fdfa9288ecb68453fced0a06c8109
fabri10roman <fabri11roman.fr@gmail.com>
2025-10-08 17:17:05 -0300

    implement create movie

--- src/features/movies/contexts/MoviesProvider.tsx
+++ src/features/movies/contexts/MoviesProvider.tsx
@@ -7,8 +7,16 @@ const STORAGE_KEY = "movies";
 
 export const MoviesProvider = ({ children }: { children: React.ReactNode }) => {
   const [movies, setMovies] = useLocalStorage<Movie[]>(STORAGE_KEY, seedMovies);
+  const createMovie = (movieData: Omit<Movie, "id">) => {
+    const newMovie = {
+      ...movieData,
+      id: Math.max(0, ...movies.map((m) => m.id)) + 1,
+    };
+    setMovies([...movies, newMovie]);
+  };
+
   return (
-    <MoviesContext.Provider value={{ movies, setMovies }}>
+    <MoviesContext.Provider value={{ movies, setMovies, createMovie }}>
       {children}
     </MoviesContext.Provider>
   );
